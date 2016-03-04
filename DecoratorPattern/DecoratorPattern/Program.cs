cca25a6f0d6d35d0d29379897b1ad8ea79afd1b5
halevk <halevk@gmail.com>
2016-03-04 09:54:41 +0200

    revised version

--- DecoratorPattern/DecoratorPattern/Program.cs
+++ DecoratorPattern/DecoratorPattern/Program.cs
@@ -17,7 +17,6 @@ namespace DecoratorPattern
             var mush = new PizzaWithMushroom("Mushroom", 20, vegPizza);
             var pep = new PizzaWithPeproni("Red Peperoni", 10, mush);
             var ced = new PizzaWithCheddarChesee("Cheddar cheese extra", 5, pep);
-
             
             Console.WriteLine("Total price of order : {0} TL", ced.Price);
             Console.WriteLine("Pizza ordered : {0}",ced.Name);
